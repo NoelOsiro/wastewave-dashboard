93747836220643846e98e3e86c49214839b1bf8a
NoelOsiro <noel.osiro@azubiafrica.org>
2025-05-19 02:20:02 +0300

    removed user from topbar sidebar

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -16,10 +16,11 @@ export async function Layout({ children, initialSidebarOpen = true }: LayoutProp
 
       <div className="h-screen flex flex-col overflow-hidden">
         <TooltipProvider delayDuration={200}>
-          <div className="fixed inset-0 bg-background z-10" />        <Topbar initialSidebarOpen={initialSidebarOpen} user={user} />
+          <div className="fixed inset-0 bg-background z-10" />        
+          <Topbar initialSidebarOpen={initialSidebarOpen}  />
 
         <div className="flex flex-1 overflow-hidden">
-          <Sidebar initialSidebarOpen={initialSidebarOpen} user={user} />
+          <Sidebar initialSidebarOpen={initialSidebarOpen}/>
 
           <main className={`flex-1 overflow-y-auto transition-all duration-300 ease-in-out bg-background ${initialSidebarOpen ? 'lg:ml-64' : 'ml-0'}`}>
             <div className="container mx-auto py-6 px-4 md:px-6 animate-fade-in">
